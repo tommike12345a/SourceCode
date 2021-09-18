eacac33e70b4612753b64c39f7f568c13312113a
tommike12345a <tommike12345a@gmail.com>
2021-09-18 16:25:37 -0400

    line 3
    line 3

--- AssemblyInfo.cs
+++ AssemblyInfo.cs
@@ -3,6 +3,8 @@ using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 //A new line added
 //Comment Line 2
+//Comment Line 3
+
 
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
