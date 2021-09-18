07d6226309b5b32be431b526c99a0579033128de
tommike12345a <tommike12345a@gmail.com>
2021-09-18 16:34:17 -0400

    Comment 5
    5th comment

--- AssemblyInfo.cs
+++ AssemblyInfo.cs
@@ -5,7 +5,7 @@ using System.Runtime.InteropServices;
 //Comment Line 2
 //Comment Line 3
 //Comment Line 4
-
+//Comment Line 5
 
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
